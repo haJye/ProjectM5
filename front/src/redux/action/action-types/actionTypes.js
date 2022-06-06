6eafa14d46f9f9ee4584994dde0f4dfa7b8576fe
haJy <65567064+haJye@users.noreply.github.com>
2022-06-06 11:22:04 +0400

    final

--- front/src/redux/action/action-types/actionTypes.js
+++ front/src/redux/action/action-types/actionTypes.js
@@ -1,9 +1,7 @@
 export const ADD_TO_CART = "ADD_TO_CART";
 export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
 
-// actions for feeback
-export const ADD_FEEDBACK = "ADD_FEEDBACK";
-export const REMOVE_FEEBACK = "REMOVE_FEEBACK";
+
 
 // actions for checkout
 export const ADD_TO_CHECKOUT = "ADD_TO_CHECKOUT";
